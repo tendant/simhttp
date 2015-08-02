8e685cafc1335c9d1abcfa8289a855713b150b92
Lei <tendant@gmail.com>
2015-08-01 19:40:08 -0700

    add support for GET/POST/PUT "/"

--- src/simhttp/handler.clj
+++ src/simhttp/handler.clj
@@ -1,11 +1,22 @@
 (ns simhttp.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults api-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [:as request]
+       (do
+         (println request)
+         "GET"))
+  (POST "/" [:as request]
+        (do
+          (println request)
+          "POST"))
+  (PUT "/" [:as request]
+        (do
+          (println request)
+          "PUT"))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes api-defaults))
